a0e892fb3d34a35fa5fc016e1e0d63601dc4110c
Ulises <ulises.torrella@gmail.com>
2016-08-31 16:29:47 -0300

    sidenav

--- app/js/app.js
+++ app/js/app.js
@@ -3,4 +3,13 @@ angular.module('Presentacion', ['ngMaterial'])
   $mdThemingProvider.theme('default')
     .primaryPalette('pink')
     .accentPalette('orange');
-});
+})
+.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function () {
+      // Component lookup should always be available since we are not using `ng-if`
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
+  });
